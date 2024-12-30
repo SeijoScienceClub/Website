44a76c78c61f7ab0a2b9718438b01a3dae84af00
kota6-me <contact@kota6.me>
2024-12-31 00:26:14 +0900

    a

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,9 @@
-export default function DashBoard() {
+import { SessionProvider } from "next-auth/react";
+
+export default function App() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 }
--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,18 +1,10 @@
 "use client";
 import { redirect, useSearchParams } from "next/navigation";
-import discordLogin from "../utils/discordLogin";
 
 export default function Redirect() {
   let searchParams = useSearchParams();
   let code = searchParams.get("code");
-  discordLogin(code).then((userData) => {
-    if (userData.isSuccessfull) {
-      redirect("/dashboard");
-    } else {
-      console.error(userData.errorDescription);
-      redirect("/error");
-    }
-  });
+
   return (
     <div>
       <h1>Now Redirecting...</h1>
